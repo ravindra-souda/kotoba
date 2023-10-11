8c0e04e723579839b93c31dba408c6f8e03c7da8
RAVINDRA Soudakar <ravindra.soudakar@free.fr>
2023-10-11 15:33:37 +0200

    build: enforce strict_types declaration

--- src/GitHook/Rules/LooselyConventionalCommits.php
+++ src/GitHook/Rules/LooselyConventionalCommits.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\GitHook\Rules;
 
 use CaptainHook\App\Hook\Message\Rule;
--- src/GitHook/StageBackLintedFiles.php
+++ src/GitHook/StageBackLintedFiles.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\GitHook;
 
 use CaptainHook\App\Config;
